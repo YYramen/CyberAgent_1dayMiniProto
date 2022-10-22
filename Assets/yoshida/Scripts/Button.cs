ab11eabe146a6b3b62f6aad15c4b327c812eda63
YusukeYoshida <87760101+YYramen@users.noreply.github.com>
2022-10-22 15:30:43 +0900

    [update] GameManagerクラス完成

--- Assets/yoshida/Scripts/Button.cs
+++ Assets/yoshida/Scripts/Button.cs
@@ -8,4 +8,32 @@ using UnityEngine;
 public class Button : MonoBehaviour
 {
     GameManager _gameMan;
+
+    [SerializeField, Tooltip("操作説明のパネル")] GameObject _instructionPanel;
+    [SerializeField, Tooltip("クレジットのパネル")] GameObject _creditPanel;
+
+    /// <summary>
+    /// 操作説明のパネルを表示
+    /// </summary>
+    public void ShowInstruction()
+    {
+        _instructionPanel.SetActive(true);
+    }
+
+    /// <summary>
+    /// クレジットの画面を表示
+    /// </summary>
+    public void ShowCredit()
+    {
+        _creditPanel.SetActive(true);
+    }
+
+    /// <summary>
+    /// メニュー画面に戻る
+    /// </summary>
+    public void ReturnMenu()
+    {
+        _instructionPanel.SetActive(false);
+        _creditPanel.SetActive(false);
+    }
 }
